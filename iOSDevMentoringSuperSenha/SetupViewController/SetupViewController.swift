40bf5b3d04e0ea8f26b73a0770ee849c683b0f14
Pedro Gabriel <pee.torres@gmail.com>
2021-05-07 20:34:49 -0300

    Created SetupViewController without storyboard.

---
+++ iOSDevMentoringSuperSenha/SetupViewController/SetupViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SetupViewController.swift
+//  iOSDevMentoringSuperSenha
+//
+//  Created by Pedro Gabriel on 07/05/21.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
